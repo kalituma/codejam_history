fee7653f7bbf8d86ceaf209adeca8433997a485c
Hyung Woo Kim <kalituma@gmail.com>
2021-05-01 05:44:27 +0900

    update and add 2018 round2 sources and screenshots

---
+++ 2018/round_2/costume_change.cpp
@@ -0,0 +1,55 @@
+// from https://github.com/ruippeixotog/google-code-jam-2018/blob/master/round2/costume-change.cpp
+#include <cstdio>
+#include <cstring>
+
+#define MAXN 100
+
+int n, a[MAXN][MAXN];
+
+bool seen[MAXN];
+int matchL[MAXN], matchR[MAXN];
+
+bool bpmDfs(int k, int u) {
+    for(int v = 0; v < n; v++) {
+        if(a[u][v] == k) {
+            if(seen[v]) continue;
+            seen[v] = true;
+
+            if(matchR[v] < 0 || bpmDfs(k, matchR[v])) {
+                matchL[u] = v; matchR[v] = u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int bpm(int k) {
+    memset(matchL, -1, sizeof(matchL));
+    memset(matchR, -1, sizeof(matchR));
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        memset(seen, false, sizeof(seen));
+        if(bpmDfs(k, i)) cnt++;
+    }
+    return cnt;
+}
+
+int main() {
+    int t; scanf("%d\n", &t);
+    for(int tc = 1; tc <= t; tc++) {
+        scanf("%d\n", &n);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int aij; scanf("%d", &aij);
+                a[i][j] = aij < 0 ? -aij - 1 : n + aij - 1;
+            }
+        }
+        int correct = 0;
+        for(int k = 0; k < 2 * n; k++) {
+            correct += bpm(k);
+        }
+        printf("Case #%d: %d\n", tc, n * n - correct);
+    }
+    return 0;
+}
---
+++ 2018/round_2/falling_balls.cpp
@@ -0,0 +1,49 @@
+// from https://github.com/ruippeixotog/google-code-jam-2018/blob/master/round2/falling-balls.cpp
+
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+
+#define MAXN 100
+
+using namespace std;
+
+int b[MAXN];
+
+char grid[MAXN][MAXN];
+
+int main() {
+    int t; scanf("%d\n", &t);
+    for(int tc = 1; tc <= t; tc++) {
+        int c; scanf("%d\n", &c);
+        for(int i = 0; i < c; i++)
+            scanf("%d", &b[i]);
+
+        if(b[0] == 0 || b[c - 1] == 0) {
+            printf("Case #%d: IMPOSSIBLE\n", tc);
+        } else {
+            memset(grid, '.', sizeof(grid));
+
+            int maxRows = 0, supply = 0;
+            for(int i = 0; i < c; i++) {
+                maxRows = max(maxRows, -supply);
+                for(int j = 0; j < -supply; j++) {
+                    grid[j][i] = '/';
+                }
+                supply += 1 - b[i];
+                maxRows = max(maxRows, supply);
+                for(int j = 0; j < supply; j++) {
+                    grid[j][i] = '\\';
+                }
+            }
+            printf("Case #%d: %d\n", tc, maxRows + 1);
+            for(int i = 0; i < maxRows + 1; i++) {
+                for(int j = 0; j < c; j++) {
+                    printf("%c", grid[i][j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
